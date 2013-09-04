4fcfd34b44e44089e1acaee33d84776e4a40dd1e
Ed Solis <edsfocci@gmail.com>
2013-09-04 14:19:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwoopoClone::Application.config.secret_token = '7a6c848d38f6ff04d52fab972d1e0fa651f24de74dbc7f8467d852f7b4767162236e2a33923094af0233ae42e4320fa062b1d1b3da91b7359d1fd4c8640d92e3'
